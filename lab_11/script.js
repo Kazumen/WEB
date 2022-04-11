7ccfd4092a30072c86f0484610834ee6a7322bba
unknown <yura07042004@gmail.com>
2022-04-12 00:29:00 +0200

    lab_11 done

---
+++ lab_11/script.js
@@ -0,0 +1,54 @@
+// 2 завдання
+console.log("Romanchak");
+
+// 3 завдання
+let data = 20;
+let data1=25;
+console.log(data,data1);
+data=data1;
+console.log(data,data1);
+
+// 4 завдання
+const newObj = {
+    String:"Banana",
+    Number:25,
+    Boolean:true,
+    Undefined:undefined,
+    Null:null
+};
+
+// 5 завдання
+let isAdult = confirm("Are you adult?");
+console.log(isAdult);
+
+// 6 завдання
+let name="Yura";
+let surname="Romanchak";
+let group="КН-321";
+let birthYear=2004;
+let isMarried=false;
+
+console.log(birthYear);
+console.log(isMarried);
+console.log(name);
+console.log(surname);
+console.log(group);
+
+let undefined1=undefined;
+let null1 = null;
+
+console.log(typeof undefined1);
+console.log(typeof null1);
+
+// 7 завдання
+let login=prompt("Введіть логін");
+let email=prompt("Введіть email");
+let password=prompt("Введіть пароль");
+
+alert("Dear " + login + " your email is " + email + ", your password is " + password);
+
+// 8 завдання
+let secondsInHour=60*60;
+let secondsInDay=24*secondsInHour;
+let secondsInMonth=28*secondsInDay;
+alert("Seconds in hour: " + secondsInHour + "\n" + "Seconds in day: " + secondsInDay + "\n" + "Seconds in month: " + secondsInMonth);
